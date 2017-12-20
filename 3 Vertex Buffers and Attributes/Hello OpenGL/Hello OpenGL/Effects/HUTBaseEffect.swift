2edd33928383014f28d460521bb078772e737734
stoull <chencc@linkapp.cn>
2017-12-20 21:28:21 +0800

    Add glsl file

---
+++ 3 Vertex Buffers and Attributes/Hello OpenGL/Hello OpenGL/Effects/HUTBaseEffect.swift
@@ -0,0 +1,20 @@
+//
+//  HUTBaseEffect.swift
+//  Hello OpenGL
+//
+//  Created by linkapp on 20/12/2017.
+//  Copyright © 2017 hut. All rights reserved.
+//
+
+import UIKit
+
+class HUTBaseEffect: NSObject {
+
+    public init(vertextShader: String, fragmentShader: String) {
+        
+    }
+    
+    public func prepareToDraw() {
+        
+    }
+}
