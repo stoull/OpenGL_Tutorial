621624c3879070233f33603243e2161b8bb6ad26
stoull <chencc@linkapp.cn>
2018-01-03 20:33:45 +0800

    A

--- 4 Shaders and the Graphics Pipeline/HelloOpenGL/HelloOpenGL/ViewController.swift
+++ 4 Shaders and the Graphics Pipeline/HelloOpenGL/HelloOpenGL/ViewController.swift
@@ -21,11 +21,25 @@ class ViewController: GLKViewController {
     }
     
     func setupVertexBuffer() {
-        let vertices: [HUTVertex] = [HUTVertex.init(Position: (1.0, -1.0, 0.0), Color: (1, 0, 0, 1)),
-                                     HUTVertex.init(Position: (1.0, 1.0, 0.0), Color: (0, 1, 0, 1)),
-                                     HUTVertex.init(Position: (-1.0, 1, 0.0), Color: (0, 0, 1, 1)),
-                                     HUTVertex.init(Position: (-1.0, -1.0, 0.0), Color: (0, 0, 0, 0))]
-        let indices: [GLubyte] = [0, 1, 2, 2, 3, 0]
+//        let vertices: [HUTVertex] = [HUTVertex.init(Position: (1, -1, 0), Color: (1, 0, 0, 1)),
+//                                     HUTVertex.init(Position: (1, 1, 0), Color: (0, 1, 0, 1)),
+//                                     HUTVertex.init(Position: (-1, 1, 0), Color: (0, 0, 1, 1)),
+//                                     HUTVertex.init(Position: (-1, -1, 0), Color: (0, 0, 0, 0))]
+        
+//        let vertices: [HUTVertex] = [HUTVertex.init(Position: (1, -1, 0, 1, 0, 0, 1)),
+//                                     HUTVertex.init(Position: (1, 1, 0, 0, 1, 0, 1)),
+//                                     HUTVertex.init(Position: (-1, 1, 0, 0, 0, 1, 1)),
+//                                     HUTVertex.init(Position: (-1, -1, 0, 0, 0, 0, 0))]
+        
+        let vertices : [HUTVertex] = [
+            HUTVertex( 1.0, -1.0, 0, 1.0, 0.0, 0.0, 1.0),
+            HUTVertex( 1.0,  1.0, 0, 0.0, 1.0, 0.0, 1.0),
+            HUTVertex(-1.0,  1.0, 0, 0.0, 0.0, 1.0, 1.0),
+            HUTVertex(-1.0, -1.0, 0, 1.0, 1.0, 0.0, 1.0)
+        ]
+        
+        let indices: [GLubyte] = [0, 1, 2,
+                                  2, 3, 0]
         
         indexCount = GLsizei(MemoryLayout.size(ofValue: indices) / MemoryLayout.size(ofValue: indices[0]))
         
@@ -57,17 +71,21 @@ class ViewController: GLKViewController {
         
         shader?.prepareToDraw()
         
-        let positionStrideSize = MemoryLayout<GLfloat>.size * 3
+        let strideSize = MemoryLayout<HUTVertex>.size
+        let positionStrideSize = MemoryLayout<GLfloat>.size * 0
         glEnableVertexAttribArray(HUTVertextAttributes.position.rawValue)
-        glVertexAttribPointer(HUTVertextAttributes.position.rawValue, 3, GLenum(GL_FLOAT), GLboolean(GLenum(GL_FALSE)), GLsizei(positionStrideSize), BUFFER_OFFSET(n: positionStrideSize))
+        glVertexAttribPointer(HUTVertextAttributes.position.rawValue, 3, GLenum(GL_FLOAT), GLboolean(GL_FALSE), GLsizei(strideSize), BUFFER_OFFSET(n: positionStrideSize))
         
         glEnableVertexAttribArray(HUTVertextAttributes.color.rawValue)
-        let colorStrideSize = MemoryLayout<GLfloat>.size * 4
-        glVertexAttribPointer(HUTVertextAttributes.color.rawValue, 4, GLenum(GL_FLOAT), GLboolean(GLenum(GL_FALSE)), GLsizei(positionStrideSize), BUFFER_OFFSET(n: colorStrideSize))
+        let colorStrideSize = MemoryLayout<GLfloat>.size * 3
+        glVertexAttribPointer(HUTVertextAttributes.color.rawValue, 4, GLenum(GL_FLOAT), GLboolean(GL_FALSE), GLsizei(strideSize), BUFFER_OFFSET(n: colorStrideSize))
         
         glBindBuffer(GLenum(GL_ARRAY_BUFFER), vertextBuffer)
         glBindBuffer(GLenum(GL_ELEMENT_ARRAY_BUFFER), indexBuffer)
+        
         glDrawElements(GLenum(GL_TRIANGLES), indexCount, GLenum(GL_UNSIGNED_BYTE), nil)
+        
+//        glDrawArrays(GLenum(GL_TRIANGLES), 0, 3);
         
         glDisableVertexAttribArray(HUTVertextAttributes.position.rawValue)
         glDisableVertexAttribArray(HUTVertextAttributes.color.rawValue)
@@ -77,9 +95,28 @@ class ViewController: GLKViewController {
         return UnsafeRawPointer(bitPattern: n)
     }
     
+//    func BUFFER_OFFSET(n: Int) -> UnsafeRawPointer? {
+//
+////        return UnsafeRawPointer(bitPattern: n)
+//
+//        var t = 0
+//        let ptr = UnsafeRawPointer.init(&t)
+//        return ptr
+//
+//        //        let pointer: UnsafeRawPointer
+////        let ptr = UnsafeRawPointer(bitPattern: n)
+//        //        let adPtr = ptr?.advanced(by: n)
+//        //        let adPtr = pointer + n
+//
+////        var f: GLfloat = 0
+////        withUnsafePointer(to: &f) { (pointer) -> Void in
+////
+////        }
+//    }
+    
 //    func BUFFER_OFFSET(n: Int) -> UnsafeRawPointer {
 //        let ptr: UnsafeRawPointer? = nil
-//        return ptr! + n
+//        return ptr?.advanced(by: n)
 //    }
 
     override func didReceiveMemoryWarning() {
