35112d28d2613cbbd6f74d8876d0a007e2ad92e8
Hut <chenchangchun@foxmail.com>
2018-01-04 23:39:00 +0800

    Add model file

---
+++ 5 Vertex Array Objects and a Model Class/HelloOpenGL/HelloOpenGL/Model/HUTModel.swift
@@ -0,0 +1,13 @@
+//
+//  HUTModel.swift
+//  HelloOpenGL
+//
+//  Created by Stoull Hut on 04/01/2018.
+//  Copyright © 2018 CCApril. All rights reserved.
+//
+
+import UIKit
+
+class HUTModel: NSObject {
+
+}
---
+++ 5 Vertex Array Objects and a Model Class/HelloOpenGL/HelloOpenGL/Model/HUTSquare.swift
@@ -0,0 +1,13 @@
+//
+//  HUTSquare.swift
+//  HelloOpenGL
+//
+//  Created by Stoull Hut on 04/01/2018.
+//  Copyright © 2018 CCApril. All rights reserved.
+//
+
+import UIKit
+
+class HUTSquare: NSObject {
+
+}
