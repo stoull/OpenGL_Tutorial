9f6f6dbeaa0233467698103c93a311f54fcd79c9
Hut <chenchangchun@foxmail.com>
2018-01-06 16:47:40 +0800

    Transfromation project setup shader

--- 6 3D Transformation/3DTransformation/3DTransformation/ViewController.swift
+++ 6 3D Transformation/3DTransformation/3DTransformation/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 import GLKit
 
 class ViewController: GLKViewController {
+    
+    var shader: HUTBaseEffect?
+    var square: HUTSquare?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,11 +22,22 @@ class ViewController: GLKViewController {
         
         EAGLContext.setCurrent(view.context)
         
+        setupSquare()
+    }
+    
+    func setupSquare() {
+        shader = HUTBaseEffect.init(vertexShader: "HUTSimpleVertex", fragmentShader: "HUTSimpleFragment")
+        if let realShader = shader {
+            square = HUTSquare.init(shader: realShader)
+        }else {
+            print("Setup Square ERROR!")
+        }
     }
     
     override func glkView(_ view: GLKView, drawIn rect: CGRect) {
         glClearColor(200.0/225.0, 104.0/255.0, 114.0/255.0, 1.0)
         glClear(GLbitfield(GL_COLOR_BUFFER_BIT))
+        square?.render()
     }
     
 
