9c3b9d24fa5b6952da1e8df058df0f9323e71ecd
Hut <chenchangchun@foxmail.com>
2018-01-07 15:22:53 +0800

    Add Projection Matrix Trnsformation Project

---
+++ 6 3D Transformation/3 Projection Matrix Transformation/3DTransformation/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  3DTransformation
+//
+//  Created by Stoull Hut on 06/01/2018.
+//  Copyright © 2018 CCApril. All rights reserved.
+//
+
+import UIKit
+import GLKit
+
+class ViewController: GLKViewController {
+    
+    var shader: HUTBaseEffect?
+    var square: HUTSquare?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        delegate = self
+        
+        let view: GLKView = self.view as! GLKView
+        view.context = EAGLContext.init(api: .openGLES2)!
+        
+        EAGLContext.setCurrent(view.context)
+        
+        setupSquare()
+    }
+    
+    func setupSquare() {
+        shader = HUTBaseEffect.init(vertexShader: "HUTSimpleVertex", fragmentShader: "HUTSimpleFragment")
+        if let realShader = shader {
+            square = HUTSquare.init(shader: realShader)
+//            square?.position = GLKVector3.init(v: (0.5, -0.5, 0))
+            
+        }else {
+            print("Setup Square ERROR!")
+        }
+    }
+    
+    override func glkView(_ view: GLKView, drawIn rect: CGRect) {
+        glClearColor(200.0/225.0, 104.0/255.0, 114.0/255.0, 1.0)
+        glClear(GLbitfield(GL_COLOR_BUFFER_BIT))
+        
+        let viewMatrix = GLKMatrix4MakeTranslation(0, -1, 0)
+        
+        square?.renderWithParentModelViewMatrix(parentModelViewMatrix: viewMatrix)
+        
+//        square?.render()
+    }
+    
+    //
+    func update() {
+        square?.updateWithDelta(time: timeSinceLastUpdate)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+extension ViewController: GLKViewControllerDelegate  {
+    func glkViewControllerUpdate(_ controller: GLKViewController) {
+        self.update()
+    }
+}
+
